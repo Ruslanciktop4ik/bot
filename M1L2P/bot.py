89932eb05339079f246d60a522ac25eafb8ae3b3
Ruslanciktop4ik <ruslanrp126@gmail.com>
2025-01-25 10:28:25 +0200

    Add files via upload

---
+++ M1L2P/bot.py
@@ -0,0 +1,45 @@
+from config import TOKEN
+import telebot
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+API_TOKEN = TOKEN
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+    
+
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    bot.reply_to(message, """\
+Команды:
+                 /start - старт
+                 /help - помощь
+                 /info - информация
+                 P.S тут так мало команд так как это тест бот\
+""")
+
+
+@bot.message_handler(content_types=['photo'])
+def photo_id(message):
+    bot.reply_to(message, "о прикольная фоточка")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
